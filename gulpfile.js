94cfe7f3113fc60fd361c0cdf5fe36204dafec0c
Marin Vlastelica P <marin.vlastelicap@gmail.com>
2016-07-02 12:08:40 +0200

    Added livereload to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -4,10 +4,12 @@ var uglify = require('gulp-uglify');
 var sourcemaps = require('gulp-sourcemaps');
 var del = require('del');
 var sass = require('gulp-sass')
+var livereload = require('gulp-livereload')
 
 var paths = {
   scripts: ['public/javascripts/**/*.js'],
-  scss: ['public/stylesheets/**/*.scss']
+  scss: ['public/stylesheets/**/*.scss'],
+  views: ['public/views/**/*.jsx']
 };
 
 // Not all tasks need to use streams
@@ -21,9 +23,11 @@ gulp.task('scss', function() {
 
  return gulp.src('./public/stylesheets/**/*.scss')
    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-   .pipe(gulp.dest('./public/stylesheets'));
+   .pipe(concat('bundle.min.css')).pipe(sourcemaps.write())
+   .pipe(gulp.dest('public/stylesheets/'));
 
 }); 
+
 gulp.task('scripts', ['clean'], function() {
   // Minify and copy all JavaScript (except vendor scripts)
   // with sourcemaps all the way down
@@ -37,9 +41,28 @@ gulp.task('scripts', ['clean'], function() {
 
 // Rerun the task when a file changes
 gulp.task('watch', function() {
-  gulp.watch(paths.scripts, ['scripts']);
-  gulp.watch(paths.scss, ['scss'])
+  gulp.watch(paths.scripts, ['scripts', 'reload']);
+  gulp.watch(paths.scss, ['scss', 'reload'])
+  gulp.watch(paths.views, ['reload'])
+  livereload.listen()
 });
+
+gulp.task('nodemon', function() {
+
+    nodemon({
+        script: 'index.js',
+        ext: 'js',
+    })
+
+})
+
+
+gulp.task('reload', function(){
+
+    livereload.reload()
+
+})
+
 
 // The default task (called when you run `gulp` from cli)
 gulp.task('default', ['watch', 'scripts']);
